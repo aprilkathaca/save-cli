be410d8b6f8eba3b1023df115a904f572e0fe201
April May <am@aprilmay.com>
2022-07-27 14:50:24 -0600

    verification

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,22 @@ include("save-plugins:warn-plugin")
 include("save-reporters")
 include("save-common-test")
 
+plugins {
+    id("com.gradle.enterprise") version "3.10"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.6.5"
+}
+gradleEnterprise {
+    server = "http://ec2-18-207-131-223.compute-1.amazonaws.com"
+    allowUntrustedServer = true // unless a trusted certificate is already configured in GE buildScan {
+    buildScan {
+        publishAlways()
+        capture {
+            isTaskInputFiles = true
+        }
+        isUploadInBackground = System.getenv("CI") == null // adjust to your CI provider }
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
